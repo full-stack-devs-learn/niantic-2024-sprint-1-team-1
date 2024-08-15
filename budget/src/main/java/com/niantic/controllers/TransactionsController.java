c85671462f8912fb980c78087e55bcf8629cdcff
Hannah Patzold <hannaholivia@myyahoo.com>
2024-08-15 14:49:11 -0700

    Bootswatch Template

---
+++ budget/src/main/java/com/niantic/controllers/TransactionsController.java
@@ -0,0 +1,30 @@
+package com.niantic.controllers;
+
+import com.niantic.services.TransactionDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+//@Controller
+//public class TransactionsController
+//{ //private TransactionDao transactionDao = new TransactionDao();
+    //@GetMapping("/transactions")
+    //public String getAllTransactions(Model model){
+        //ArrayList<Transactions> transactions;
+//        transactions = transactionDao.getTransactionById();
+//        model.addAttribute("transactions", transactions);
+//        return "transactions/index";
+
+   // }
+
+
+
+//    @GetMapping("/")
+//    public String transaction(Model model)
+//    {
+//        model.addAttribute("pageTitle", "Budget Pal :: Transaction");
+//        return "index";
+//    }
+//}
