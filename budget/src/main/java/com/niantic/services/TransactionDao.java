98e8b0fd33b0256197646355ee2462c3db4636a8
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-15 12:09:00 -0700

    Created getTransactionsBy() functions

--- budget/src/main/java/com/niantic/services/TransactionDao.java
+++ budget/src/main/java/com/niantic/services/TransactionDao.java
@@ -1,7 +1,14 @@
 package com.niantic.services;
 
+import com.niantic.models.Transaction;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import java.time.LocalDate;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class TransactionDao {
     private final JdbcTemplate jdbcTemplate;
@@ -10,7 +17,7 @@ public class TransactionDao {
         String databaseUrl = "jdbc:mysql://localhost:3306/budget";
         String userName = "root";
         String password = "P@ssword";
-        DataSource datasource = new BasicDataSource(){{
+        DataSource dataSource = new BasicDataSource(){{
             setUrl(databaseUrl);
             setUsername(userName);
             setPassword(password);
@@ -19,21 +26,232 @@ public class TransactionDao {
         JdbcTemplate(dataSource);
     }
 // + getTransactionByUser(userId: int): ArrayList<Transaction>
- public ArrayList<Transaction> getTransactionByUser (int userId){
+     public ArrayList<Transaction> getTransactionByUser (int userId)
+     {
+        ArrayList<Transaction> transactions = new ArrayList<>();
 
- }
+        String sql = """
+                SELECT transaction_id
+                    , user_id
+                    , category_id
+                    , vendor_id
+                    , transaction_date
+                    , amount
+                    , notes
+                FROM transactions
+                WHERE transaction_id = ?;
+                """;
+
+         SqlRowSet row = jdbcTemplate.queryForRowSet(sql, userId);
+
+         while(row.next())
+         {
+             int transactionId = row.getInt("transaction_id");
+             int categoryId = row.getInt("category_id");
+             int vendorId = row.getInt("vendor_id");
+             LocalDate transactionDate = row.getDate("transaction_date").toLocalDate();
+             double amount = row.getDouble("amount");
+             String notes = row.getString("notes");
+
+             Transaction transaction = new Transaction(transactionId, userId, categoryId, vendorId, transactionDate, amount, notes);
+             transactions.add(transaction);
+         }
+         return transactions;
+     }
+
 // +getTransactionByMonth(month: int): ArrayList<Transaction>
+    public ArrayList<Transaction> getTransactionByMonth(String month)
+    {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        month = "%-" + month + "-%";
+
+        String sql = """
+                SELECT transaction_id
+                    , user_id
+                    , category_id
+                    , vendor_id
+                    , transaction_date
+                    , amount
+                    , notes
+                FROM transactions
+                WHERE transaction_date = ?;
+                """;
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, month);
+
+        while(row.next())
+        {
+            int transactionId = row.getInt("transaction_id");
+            int userId = row.getInt("user_id");
+            int categoryId = row.getInt("category_id");
+            int vendorId = row.getInt("vendor_id");
+            LocalDate transactionDate = row.getDate("transaction_date").toLocalDate();
+            double amount = row.getDouble("amount");
+            String notes = row.getString("notes");
+
+            Transaction transaction = new Transaction(transactionId, userId, categoryId, vendorId, transactionDate, amount, notes);
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
 
 // + getTransactionByYear(year: int): ArrayList<Transaction>
+    public ArrayList<Transaction> getTransactionByYear(String year)
+    {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        year = year + "-%";
+
+        String sql = """
+                SELECT transaction_id
+                    , user_id
+                    , category_id
+                    , vendor_id
+                    , transaction_date
+                    , amount
+                    , notes
+                FROM transactions
+                WHERE transaction_date = ?;
+                """;
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, year);
+
+        while(row.next())
+        {
+            int transactionId = row.getInt("transaction_id");
+            int userId = row.getInt("user_id");
+            int categoryId = row.getInt("category_id");
+            int vendorId = row.getInt("vendor_id");
+            LocalDate transactionDate = row.getDate("transaction_date").toLocalDate();
+            double amount = row.getDouble("amount");
+            String notes = row.getString("notes");
+
+            Transaction transaction = new Transaction(transactionId, userId, categoryId, vendorId, transactionDate, amount, notes);
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
 
 // + getTransactionByCategory(categoryId: int): ArrayList<Transaction>
+    public ArrayList<Transaction> getTransactionByCategory(int categoryId)
+    {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        String sql = """
+                SELECT transaction_id
+                    , user_id
+                    , category_id
+                    , vendor_id
+                    , transaction_date
+                    , amount
+                    , notes
+                FROM transactions
+                WHERE category_id = ?;
+                """;
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, categoryId);
+
+        while(row.next())
+        {
+            int transactionId = row.getInt("transaction_id");
+            int userId = row.getInt("user_id");
+            int vendorId = row.getInt("vendor_id");
+            LocalDate transactionDate = row.getDate("transaction_date").toLocalDate();
+            double amount = row.getDouble("amount");
+            String notes = row.getString("notes");
+
+            Transaction transaction = new Transaction(transactionId, userId, categoryId, vendorId, transactionDate, amount, notes);
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
 
 // +getTransactionById(transactionId: int): Transaction
+    public Transaction getTransactionById(int transactionId)
+    {
+        Transaction transaction = new Transaction();
+
+        String sql = """
+                SELECT transaction_id
+                    , user_id
+                    , category_id
+                    , vendor_id
+                    , transaction_date
+                    , amount
+                    , notes
+                FROM transactions
+                WHERE transaction_id = ?;
+                """;
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, transactionId);
+
+        while(row.next())
+        {
+            int userId = row.getInt("user_id");
+            int categoryId = row.getInt("category_id");
+            int vendorId = row.getInt("vendor_id");
+            LocalDate transactionDate = row.getDate("transaction_date").toLocalDate();
+            double amount = row.getDouble("amount");
+            String notes = row.getString("notes");
+
+            transaction = new Transaction(transactionId, userId, categoryId, vendorId, transactionDate, amount, notes);
+
+        }
+        return transaction;
+    }
 
 // +addTransaction(transaction: Transaction): void
+    public void addTransaction(Transaction transaction)
+    {
+        String sql = """
+                INSERT INTO transactions (transaction_id, user_id, category_id, vendor_id, transaction_date, amount, notes)
+                VALUES (?,?,?,?,?,?,?);
+                """;
+
+        jdbcTemplate.update(sql
+                , transaction.getTransactionId()
+                , transaction.getUserId()
+                , transaction.getCategoryId()
+                , transaction.getVendorId()
+                , transaction.getTransactionDate()
+                , transaction.getAmount()
+                , transaction.getNotes());
+    }
 
 // + updateTransaction(transaction: Transaction): void
+    public void updateTransaction(Transaction transaction)
+    {
+        String sql = """
+                UPDATE transactions
+                SET transaction_id = ?
+                    , user_id = ?
+                    , category_id = ?
+                    , vendor_id = ?
+                    , transaction_date = ?
+                    , amount = ?
+                    , notes = ?
+                WHERE transaction_id = ?;
+                """;
+
+        jdbcTemplate.update(sql
+                , transaction.getTransactionId()
+                , transaction.getUserId()
+                , transaction.getCategoryId()
+                , transaction.getVendorId()
+                , transaction.getTransactionDate()
+                , transaction.getAmount()
+                , transaction.getNotes());
+    }
 
 // + deleteTransaction(transactionId: int): void
+    public void deleteTransaction(int transactionId)
+    {
+        String sql = """
+                DELETE FROM transactions
+                WHERE transaction_id = ?
+                """;
 
+        jdbcTemplate.update(sql, transactionId);
+    }
 }
