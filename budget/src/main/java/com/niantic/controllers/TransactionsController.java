f8a789492d0bb80581bc227ffd5f078bd4597e7e
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-16 14:33:05 -0700

    Created add_edit transaction page

--- budget/src/main/java/com/niantic/controllers/TransactionsController.java
+++ budget/src/main/java/com/niantic/controllers/TransactionsController.java
@@ -5,9 +5,9 @@ import com.niantic.services.TransactionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.font.TransformAttribute;
 import java.util.ArrayList;
 
 @Controller
@@ -40,4 +40,35 @@ public class TransactionsController {
         model.addAttribute("transactions", transactions);
         return "transactions/reports";
     }
+
+    //Add Transaction
+    @GetMapping("transactions/add")
+    public String addTransaction(Model model)
+    {
+        model.addAttribute("transaction", new Transaction());
+        model.addAttribute("action", "add");
+        return "transactions/add_edit";
+    }
+
+    @PostMapping("transactions/add")
+    public String addTransaction(Model model, @ModelAttribute("transaction") Transaction transaction)
+    {
+        transactionDao.addTransaction(transaction);
+        model.addAttribute("transaction", transaction);
+        return "transactions/add_success";
+    }
+
+    //Error page
+//    @GetMapping("transactions/404")
+//    public String errorPage(Model model)
+//    {
+//        return "transactions/404";
+//    }
+//
+//    @PostMapping("transactions/404")
+//    public String errorPage(Model model)
+//    {
+//        return "redirect:/reports";
+//    }
+//
 }
